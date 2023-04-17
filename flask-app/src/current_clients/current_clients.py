de7950f9fce1ce641d22d070d835888d38c955ea
Jane <ytzheng2003@gmail.com>
2023-04-17 12:49:57 -0400

    Added persona 1

---
+++ flask-app/src/current_clients/current_clients.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+
+customers = Blueprint('current_clients', __name__)
+
+# Get all customer information from the DB
+@customers.route('/customer_information', methods=['GET'])
+def get_customers():
+    cursor = db.get_db().cursor()
+    cursor.execute('select Address, Phone,\
+        contact_title, first_name, last_name, demographics, CI_Customer_ID,\
+                   O_Order_ID from customer_information')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# # Get customer detail for customer with particular userID
+# @customers.route('/customers/<userID>', methods=['GET'])
+# def get_customer(userID):
+#     cursor = db.get_db().cursor()
+#     cursor.execute('select * from customers where id = {0}'.format(userID))
+#     row_headers = [x[0] for x in cursor.description]
+#     json_data = []
+#     theData = cursor.fetchall()
+#     for row in theData:
+#         json_data.append(dict(zip(row_headers, row)))
+#     the_response = make_response(jsonify(json_data))
+#     the_response.status_code = 200
+#     the_response.mimetype = 'application/json'
+#     return the_response
